52688e2827532ae3a24cc9be2a0921a50b3988c1
Kai Häppeler <kahait01@hs-esslingen.de>
2021-05-05 15:46:51 +0200

    add trainLabelId.png to changeFIlenames.m

--- changeFilenames.m
+++ changeFilenames.m
@@ -9,9 +9,9 @@ rename=input('new file name: ', 's');
 offset_idx=input('offset for the index in new filename: ');
 offset_idx = cast(offset_idx,'uint32');
 disp('Raw or annotations img?')
-rawOri=input('0: raw, 1:annotations: ');
+rawOri=input('0: raw, 1: annotations, 2: labelTrainIds : ');
 
-if ((rawOri == 0) || (rawOri == 1))== 0
+if ((rawOri == 0) || (rawOri == 1)|| (rawOri == 2))== 0
     return;
 end
 
@@ -30,9 +30,16 @@ for id = 1:length(files)
                 disp(strcat(path, files(id).name, ' already exists'));
             end
                 
-        else %annotations
+        elseif rawOri == 1 %annotations
             if exist(strcat(path, rename, '_', num2str(id, '%05d'), '_', 'polygons', ext), 'file')== 0 %does not already exist
                 movefile(strcat(path, files(id).name), strcat(path, rename, '_', num2str(id+offset_idx, '%05d'), '_', 'polygons', ext), 'f');
+            else %already exists
+                disp(strcat(path, files(id).name, ' already exists'));
+            end
+        
+        elseif rawOri == 2 %labelTrainIds
+            if exist(strcat(path, rename, '_', num2str(id, '%05d'), '_', 'labelTrainIds', ext), 'file')== 0 %does not already exist
+                movefile(strcat(path, files(id).name), strcat(path, rename, '_', num2str(id+offset_idx, '%05d'), '_', 'labelTrainIds', ext), 'f');
             else %already exists
                 disp(strcat(path, files(id).name, ' already exists'));
             end
