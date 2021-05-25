bcf92faef780c761a713d3b06e640de49cc52927
Kai Häppeler <kahait01@hs-esslingen.de>
2021-05-25 10:34:14 +0200

    update some comments

--- labeling/changeLabelDefinition.m
+++ labeling/changeLabelDefinition.m
@@ -13,6 +13,7 @@ disp(' ');
 imageSpace=input('path to the main folder, where the images are stored (z.B. ~/.../Senay/: ', 's');
 addpath(genpath(imageSpace));
 
+%create new folder for the results
 if exist(strcat(imageSpace, 'changedLabelID'), 'dir') == 0
     mkdir(strcat(imageSpace, 'changedLabelID'));
 else
@@ -23,7 +24,7 @@ end
 load(gTruth);
 pixelLabelMappingList=importValidationPixelLabelMappingList('validationPixelLabelIdMappingList.csv');
 
-%step trough all PixelLabel Images and chage the invalid labels with the
+%step trough all PixelLabel Images and change the invalid labels with the
 %valid labes
 for i=1:length(gTruth.LabelData.PixelLabelData)
     
